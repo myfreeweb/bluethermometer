e1e1b1e32491d16f65e5020a13474743837945ac
Greg V <greg@unrelenting.technology>
2020-01-01 16:26:27 +0300

    HALify timer usage (and set compare value that fits into 16 bits)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@
 use {
     byteorder::{BigEndian, ByteOrder, LittleEndian},
     fixed::types::{I30F2, I8F8},
-    nrf51_hal::temp::Temp,
+    nrf51_hal::{
+        hi_res_timer::{As16BitTimer, HiResTimer, TimerCc, TimerFrequency},
+        temp::Temp,
+    },
     panic_semihosting as _,
     rubble::{
         beacon::Beacon,
@@ -72,7 +75,7 @@ const APP: () = {
         radio: BleRadio,
         devaddr: [u8; 6],
         devaddr_type: AddressKind,
-        beacon_timer: nrf51::TIMER1,
+        beacon_timer: HiResTimer<nrf51::TIMER1, u16>,
         onboard_temp: Temp,
     }
 
@@ -85,17 +88,6 @@ const APP: () = {
             ctx.device.CLOCK.tasks_hfclkstart.write(|w| unsafe { w.bits(1) });
             while ctx.device.CLOCK.events_hfclkstarted.read().bits() == 0 {}
         }
-
-        // Configure TIMER1 as the beacon timer. It's only used as a 16-bit timer.
-        ctx.device.TIMER1.bitmode.write(|w| w.bitmode()._16bit());
-        // prescaler = 2^9    = 512
-        // 16 MHz / prescaler = 31_250 Hz
-        ctx.device.TIMER1.prescaler.write(|w| unsafe { w.prescaler().bits(9) }); // 0-9
-        ctx.device.TIMER1.intenset.write(|w| w.compare0().set());
-        ctx.device.TIMER1.shorts.write(|w| w.compare0_clear().enabled());
-        ctx.device.TIMER1.cc[0].write(|w| unsafe { w.bits(31_250 * 10) }); // ~1x per 10 second
-        ctx.device.TIMER1.tasks_clear.write(|w| unsafe { w.bits(1) });
-        ctx.device.TIMER1.tasks_start.write(|w| unsafe { w.bits(1) });
 
         let mut devaddr = [0u8; 6];
         let devaddr_lo = ctx.device.FICR.deviceaddr[0].read().bits();
@@ -112,19 +104,26 @@ const APP: () = {
 
         let radio = BleRadio::new(ctx.device.RADIO, &ctx.device.FICR, ctx.resources.ble_tx_buf, ctx.resources.ble_rx_buf);
 
+        let mut beacon_timer = ctx.device.TIMER1.as_16bit_timer();
+        beacon_timer.set_frequency(TimerFrequency::Freq31250Hz);
+        beacon_timer.set_compare_register(TimerCc::CC0, 31_250 * 2); // NOTE: 16-bit
+        beacon_timer.enable_compare_interrupt(TimerCc::CC0);
+        beacon_timer.clear();
+        beacon_timer.start();
+
         init::LateResources {
             radio,
             devaddr,
             devaddr_type,
-            beacon_timer: ctx.device.TIMER1,
             onboard_temp: Temp::new(ctx.device.TEMP),
+            beacon_timer,
         }
     }
 
     #[task(binds = TIMER1, resources = [beacon_timer, devaddr, devaddr_type, radio, onboard_temp])]
     fn timer1(ctx: timer1::Context) {
         // Acknowledge event so that the interrupt doesn't keep firing
-        ctx.resources.beacon_timer.events_compare[0].reset();
+        ctx.resources.beacon_timer.clear_compare_event(TimerCc::CC0);
 
         let addr = &*ctx.resources.devaddr;
         let addr_type = *ctx.resources.devaddr_type;
